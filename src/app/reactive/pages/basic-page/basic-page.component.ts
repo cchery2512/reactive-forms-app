430b6ef4e9c1c70a17554454b359dad533721de8
CARMELO CHERY <cchery@solucionesinformaticasmj.com>
2025-09-04 01:36:39 +0200

    COMMIT 2 FORMS

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,47 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+  private fb = inject(FormBuilder);
+  myForm: FormGroup = this.fb.group({
+    name: [
+      '',
+      [Validators.required, Validators.minLength(3)], //Validadores síncronos
+      [], //Validadores asíncronos
+    ],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
+  });
 
+  // myForm2 = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  isValidField(fieldName: string): boolean | null{
+    return !!this.myForm.controls[fieldName].errors;
+  }
+  getFieldError(fieldName: string): string | null{
+    if(!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+    if(this.myForm.controls[fieldName].pristine === false){
+      for (const key of Object.keys(errors)) {
+        switch (key) {
+          case 'required': return 'Este campo es requerido';
+          case 'minlength': return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`
+          case 'min': return `Valor mínimo de ${errors['min'].min}`
+        }
+      }
+    }
+    return null;
+  }
 }
---
+++ src/app/shared/components/form-error/form-error.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-error',
+  imports: [],
+  templateUrl: './form-error.component.html',
+  styles: ``
+})
+export class FormErrorComponent {
+
+}
